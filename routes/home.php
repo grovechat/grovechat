95db2eb4b9f8b41c79cac1eeb7a0db2b28634b27
grove1993 <grove1993.ai@gmail.com>
2025-12-28 01:33:38 +0800

    feat: 租户根路由跳转到dashboard

--- routes/home.php
+++ routes/home.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Middleware\IdentifyTenant;
+use App\Models\Tenant;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+Route::get('/', function (Tenant $tenant) {
+    return redirect()->route('tenant.dashboard', $tenant->path);
+});
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('tenant.dashboard');
