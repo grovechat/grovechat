09803c2885a06e6987622e21d5eed0ec36f5c4ea
shellphy <ai.shellphy@gmail.com>
2025-12-30 23:29:18 +0800

    fix: 上传图片接口增加认证

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/upload-image', [\App\Http\Controllers\Api\CommonController::class, 'uploadImage']);
+Route::post('/upload-image', [\App\Http\Controllers\Api\CommonController::class, 'uploadImage'])->middleware('auth:sanctum');
