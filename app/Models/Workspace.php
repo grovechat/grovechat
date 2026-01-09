3980df5595c5322fe60a02d6e719918a860e315b
grove1993 <grove1993.ai@gmail.com>
2026-01-09 20:42:35 +0800

    优化slug生成逻辑

--- app/Models/Workspace.php
+++ app/Models/Workspace.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Godruoyi\Snowflake\Snowflake;
 use Hashids\Hashids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -25,7 +26,6 @@ class Workspace extends Model
 
     public function createSlug()
     {
-        $slug = (new Hashids('', 10, 'abcdefghijklmnopqrstuvwxyz'))->encode($this->id);
-        $this->update(['slug' => $slug]);
+        $this->update(['slug' => (new Snowflake())->id()]);
     }
 }
